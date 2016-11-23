57b2e14bd2c92a89a8e951f3cad0805cfbaace24
Abhijit Shilawat <abhijits@bsf.io>
2016-11-23 12:29:44 +0530

    Do correction

---
+++ contact-form-to-excel-addon.php
@@ -0,0 +1,161 @@
+<?php 
+/*
+Plugin Name: CF7 to Spreadsheet
+Plugin URI:  http://localhost/plugin/wp-admin/plugins
+Description: Save your contact form 7 data to google spreadsheet.
+Version:     1.0
+Author:      Abhijit
+Author URI:  https://abhijits.sharkz.in
+Text Domain: cf-7-to-spreadsheet
+Domain Path: /languages
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+require_once plugin_dir_path(__FILE__).'lib/autoload.php';
+require_once plugin_dir_path(__FILE__).'lib/php-google-oauth/Google_Client.php';
+
+/**
+* Initialize the service request factory
+*/ 
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
+
+/**
+ * Add plugin in admin setting menu.
+ */
+add_action( 'admin_menu','register_cf7_to_spreadsheet_setting' );
+function register_cf7_to_spreadsheet_setting() {
+    add_submenu_page( 
+    	'options-general.php',	// Parent menu item slug
+    	__( 'CF7 to Spreadsheet','cf-7-to-spreadsheet' ),	// Page Title
+    	__( 'CF7 to Spreadsheet','cf-7-to-spreadsheet' ),	// Menu Title
+    	'manage_options',		// Capability
+    	'cf7-to-sheet',			// Menu Slug
+    	'cf7_setting_page'		// Callback function
+        );
+    add_action( 'admin_init', 'cf7_to_spreadsheet_register_setting' );
+}
+
+//register our settings
+function cf7_to_spreadsheet_register_setting() {
+	register_setting( 'cf7_to_spreadsheet_plugin_setting', 'cf7_to_spreadsheet_google_code' );
+}
+
+add_action( 'admin_init',  'cf7_to_gs_css' );
+add_action( 'admin_print_styles', 'cf7_to_gs_css' );
+/**
+ * Register and enqueue our stylesheet.
+ */
+function cf7_to_gs_css(){
+	wp_register_style( 'cf7_to_gs_style', plugins_url( "css/cf7-to-sheet-style.css",__FILE__ ) );
+	wp_enqueue_style( 'cf7_to_gs_style' );
+}
+
+
+add_action('wpcf7_after_save', 'save_cf7_gs_settings' );
+function save_cf7_gs_settings( $post ) {
+	update_post_meta( $post->id(), 'cf7_to_spreadsheet_save', $_POST['cf7-sheet'] );
+}
+/**
+* Add new tab to contact form 7 editors panel
+* @since 1.0
+*/
+add_filter( 'wpcf7_editor_panels', 'cf7_to_sheet_editor_panels'  );
+function cf7_to_sheet_editor_panels( $panels ) {
+	$panels[ 'google_sheets' ] = array(
+  	'title' => __( 'CF7 to Spreadsheet', 'cf-7-to-spreadsheet' ),
+  	'callback' =>'cf7_editor_panel_google_sheet'
+  	);
+  	return $panels;
+}
+
+function cf7_editor_panel_google_sheet( $post ) { 
+	$form_id = sanitize_text_field( $_GET['post'] );
+	$form_data = get_post_meta( $form_id, 'cf7_to_spreadsheet_save' ); ?>
+    <div class="wrap cf7-fields">
+    	<h2><?php __( 'Google Spreadsheet Settings','cf-7-to-spreadsheet' ); ?></h2>
+		<p><?php __( 'Add your spreadsheet details here.','cf-7-to-spreadsheet' ); ?></p>
+		<form  method="post" class="container">
+			<table class="form-table">
+		    	<tr valign="top">
+		        	<th scope="row"><?php _e( 'Google Spreadsheet Name','cf-7-to-spreadsheet' ); ?></th>
+		           	<td><input type="text" name="cf7-sheet[sheet-name]" class="large-text code" value="<?php echo ( isset ( $form_data[0]['sheet-name'] ) ) ? esc_attr( $form_data[0]['sheet-name'] ) : ''; ?>" />
+		        </tr>
+		        <tr valign="top">
+		        	<th scope="row"><?php _e( 'Google Spreadsheet Tab Name','cf-7-to-spreadsheet' ); ?></th>
+		           	<td><input type="text" name="cf7-sheet[sheet-tab-name]" class="large-text code" value="<?php echo ( isset ( $form_data[0]['sheet-tab-name'] ) ) ? esc_attr( $form_data[0]['sheet-tab-name'] ) : ''; ?>"/>
+		        </tr>
+		    </table>
+	  	</form>
+	</div>
+<?php }
+
+function cf7_setting_page() {?>
+	<div class="wrap">
+		<h1><?php _e( 'CF7 to Spreadsheet Setting','cf-7-to-spreadsheet' ); ?></h1>
+		<form  action="options.php" method="post" class="container">
+			<?php settings_fields( 'cf7_to_spreadsheet_plugin_setting' ); ?>
+		    <?php do_settings_sections( 'cf7_to_spreadsheet_plugin_setting' ); ?>
+		    <table class="form-table">
+		    	<tr valign="top">
+		        	<th scope="row"> <?php _e( 'Google Access Code','cf-7-to-spreadsheet' ); ?></th>
+		        	<p class="description"> <?php _e( 'Click "Get code" to retrieve your code from Google Drive to allow us to access your spreadsheets. And paste the code in the below textbox.','cf-7-to-spreadsheet' ); ?></p>
+		        	<td><input type="text" name="cf7_to_spreadsheet_google_code" required="required" value="<?php echo esc_attr( get_option('cf7_to_spreadsheet_google_code')) ?>"  /> 
+		        		<a  href="https://accounts.google.com/o/oauth2/auth?response_type=code&access_type=offline&client_id=448551536053-e36uicg9npg51m0e89kb51i37b6741fq.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&state&scope=https%3A%2F%2Fspreadsheets.google.com%2Ffeeds%2F" target="_blank" ><?php _e( 'Get Code ','cf-7-to-spreadsheet' ); ?></a>
+		           	</td>
+		        </tr>
+		    </table>
+		    <?php 
+		    if ( isset ( $_GET['settings-updated'] ) && get_option( 'cf7_to_spreadsheet_google_code' ) != '' ){
+				include_once( plugin_dir_path(__FILE__) . "lib/google-sheets.php" );
+				googlesheet::preauth( get_option('cf7_to_spreadsheet_google_code') ); 
+				//call method to preauthontication
+				update_option('cf7_to_spreadsheet_google_code', null);
+			}?>
+		    <?php submit_button();?>
+    	</form>
+	</div>  
+   <?php
+}
+
+
+add_action( 'wpcf7_mail_sent','cf7_to_spreadsheet' );
+function cf7_to_spreadsheet( $cfdata ) {
+	$submission = WPCF7_Submission::get_instance();
+	$form_id = $cfdata->id();
+	$form_data = get_post_meta( $form_id, 'cf7_to_spreadsheet_save' );
+	$sheet_name = $form_data[0]['sheet-name'];
+    $sheet_tab_name = $form_data[0]['sheet-tab-name'];
+	$my_data = array();
+    if ( $submission ) {
+    	$posted_data = $submission->get_posted_data();
+		include_once( plugin_dir_path(__FILE__) . "lib/google-sheets.php" );
+		$doc = new googlesheet();
+		$doc->auth();
+		/**
+		* Get spreadsheet by title
+		*/		
+		$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
+		$spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
+		$spreadsheet = $spreadsheetFeed->getByTitle( $sheet_name );
+		/**
+		* Get particular worksheet of the selected spreadsheet
+		*/
+		$worksheetFeed = $spreadsheet->getWorksheets();
+		$worksheet = $worksheetFeed->getByTitle( $sheet_tab_name );
+		// adding date coloumn to  your spreadsheet
+		$my_data["date"] = date('n/j/Y');
+		foreach ( $posted_data as $key => $value ) {
+			// exclude the default wpcf7 fields in object
+			// handle strings and array elements
+				if ( is_array( $value ) ) {
+					$my_data[$key] = implode( ',', $value );	
+				} else {
+					$my_data[$key] = $value;
+				}					
+			}
+		$listFeed = $worksheet->getListFeed();
+		$listFeed->insert( $my_data );
+	}
+}
--- readme.txt
+++ readme.txt
@@ -1,28 +1,48 @@
-=== Contact Form 7 To Google Sheet ===
+=== Contact Form 7 To Google Spreadsheet ===
 Contributors: brainstormforce, abhijits
 Donate link: https://www.brainstormforce.com/payment/
-Requires at least: 4.4
+Tags: google, sheets, spreadsheets, google sheets, google spreadsheets,  cf7, contact form 7, data, form, form data
+Requires at least: 
 Version:1.0
+License: 
+License URI: 
 
-
-Send your Contact Form 7 data directly to your Google Sheets spreadsheet.
+Send your Contact Form 7 data directly to your Google Spreadsheet.
 
 == Description ==
 
 This plugin connect your Contact Form 7 and Google Spreadsheet.
 
-When a visitor submits his/her data on your website via a Contact Form 7 form, on form submission, such data are added into Google Sheets.
+When a visitor submits his/her data on your website via a Contact Form 7 form, on form submission, such data are added into Google Spreadsheet.
+
+This is simple way to maintain your contact form 7 data backup.
+
+= How to Use this Plugin =
+
+*In Google Spreadsheet* 
+ 
+* Log into your Google Account and visit Google Spreadsheet.  
+* Create a new Sheet and name it.  
+* Rename the tab on which you want to capture the data.
+* In the Google sheets tab, provide column names in row 1. The first column should be "date" if you want to add date. For each further column, copy paste mail tags from the Contact Form 7 form (e.g. "your-name", "your-email", "your-subject", "your-message", etc). Also you can add required mail tags (e.g "your-name", "your-email").
+
+*In WordPress Admin*
+  
+* Create or Edit the Contact Form 7 form from which you want to capture the data. Set up the form as usual in the Form and Mail etc tabs. Thereafter, go to the new "CF7 to Spreadsheet" tab.  
+* On the "CF7 to Spreadsheet" tab, copy-paste the Google Spreadsheets sheet name and tab name into respective positions, and hit "Save".
+
+= Important Notes = 
+
+* You must pay very careful attention to your naming. This plugin will have unpredictable results if names and spellings do not match between your Google Spreadsheets and form settings. Also naming are not case sensative.
 
 == Installation ==
 
-1. Upload `cf7-to-spreadsheet` to the `/wp-content/plugins/` directory and Install`.  
+1. Upload `contact-form-to-excel-addon` to the `/wp-content/plugins/` directory and Install`.  
 2. Activate the plugin through the 'Plugins' screen in WordPress.
-3. Log in to your Google Account and create a new Sheet in which you have to send data. Also check which Sheet tab is used, usually 'Sheet1' is tab name.
-4. Add a column name in Row 1 for "date" and each form field you will have in your form (default CF7 form field names are "your-name", "your-email", "your-subject", "your-message") in next rows.
 3. Use the `Admin Panel > Setting > CF7 To spreadsheet` screen to connect to `Google Sheets Account` by entering the Access Code. You can get the Access Code by clicking the "Get Code" button. 
-4. Use the `Admin Panel > Contact form 7 > Select Contact form > On editor window - CF7 To spreadsheet` Add your Sheet Name And Tab Name 
+4. Use the `Admin Panel > Contact form 7 > Select Contact form > On editor window tab - CF7 To spreadsheet` Add your Sheet Name And Tab Name.
 
 == Changelog ==
 
 = 1.0.0 =
-- Initial release
+* Initial release
