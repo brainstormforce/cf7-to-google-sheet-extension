30fce2307b6e6f88911b5ee1c88a1087f5567ace
Abhijit Shilawat <abhijits@bsf.io>
2016-12-23 09:31:38 +0530

    Added error message

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 jQuery(document).ready(function(){
-	jQuery("checking").append('error');
-	jQuery('#setting-form').hide();
+	jQuery('#cf7-setting-form').hide();
 	jQuery('#cf7-toggle-button').click(function() {
-		jQuery('#enable_spreadsheet').toggle();
+		jQuery('#cf7-enable_spreadsheet').toggle();
 	});
-	jQuery('#google-connect').click(function() {
-		jQuery('#setting-form').show();
+	jQuery('#cf7-google-connect').click(function() {
+		jQuery('#cf7-setting-form').show();
 	});
 });
